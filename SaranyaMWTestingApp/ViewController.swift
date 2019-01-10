1b6a2e6387f99df9b9500e1f410ed3cec4924ed8
Sugunajayaseelan <46539737+Sugunajayaseelan@users.noreply.github.com>
2019-01-10 14:21:10 -0800

    First commit

---
+++ SaranyaMWTestingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaranyaMWTestingApp
+//
+//  Created by Saranya Jayaseelan on 1/10/19.
+//  Copyright © 2019 Saranya Jayaseelan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
